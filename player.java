12e298cabe8371e43042a668c6806d5f4e099d3e
inu12345 <47567133+inu12345@users.noreply.github.com>
2019-02-13 21:54:53 +0900

    Update and rename README.md to player.java

---
+++ player.java
@@ -0,0 +1,14 @@
+# -
+public class Player
+{
+	static String name = "ああああ";
+	static int lv = 30;		//レベル
+	static int hp = 30;		//HP
+	static double gold=50;		//所持金
+	
+	public static String getStatusString()
+	{
+		return("  "+name + " LV" + lv + "  HP" + hp + "  "+gold+"G");
+	}
+	
+}
